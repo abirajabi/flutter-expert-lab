07c7683a9ae4ff0dfa7c7757837a96c6a7a6b36e
Naufal Rajabi <abinaufal71@gmail.com>
2021-10-18 11:42:35 +0700

    Fix all import, updat package version on pubspec.yaml

--- lib/presentation/provider/movie_search_notifier.dart
+++ lib/presentation/provider/movie_search_notifier.dart
@@ -1,6 +1,6 @@
-import 'package:ditonton/common/state_enum.dart';
-import 'package:ditonton/domain/entities/movie.dart';
-import 'package:ditonton/domain/usecases/search_movies.dart';
+import '../../common/state_enum.dart';
+import '../../domain/entities/movie.dart';
+import '../../domain/usecases/search_movies.dart';
 import 'package:flutter/foundation.dart';
 
 class MovieSearchNotifier extends ChangeNotifier {
--- lib/presentation/provider/watchlist_movie_notifier.dart
+++ lib/presentation/provider/watchlist_movie_notifier.dart
@@ -1,6 +1,6 @@
-import 'package:ditonton/common/state_enum.dart';
-import 'package:ditonton/domain/entities/movie.dart';
-import 'package:ditonton/domain/usecases/get_watchlist_movies.dart';
+import '../../common/state_enum.dart';
+import '../../domain/entities/movie.dart';
+import '../../domain/usecases/get_watchlist_movies.dart';
 import 'package:flutter/foundation.dart';
 
 class WatchlistMovieNotifier extends ChangeNotifier {
